c204083f8985f69f536589fb311a754a3b8c8f4f
LaniMarsh <122917983+LaniMarsh@users.noreply.github.com>
2024-04-04 17:35:57 -0700

    testing

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,13 @@
+// src/MyApp.jsx
+import React from "react";
+import Table from "./Table"
+
+function MyApp() {
+  return (
+    <div className="container">
+      <Table/>
+    </div>
+  );
+}
+// makes the componenet availible to be imported into other components
+export default MyApp;
