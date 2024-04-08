7a06927fed5d1b01bcc38726b4f61bbc92a664af
LaniMarsh <122917983+LaniMarsh@users.noreply.github.com>
2024-04-08 16:22:13 -0700

    Update MyApp.jsx

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -4,22 +4,22 @@ import Table from "./Table"
 import Form from "./Form";
 
 const characters = [
-  {
-    name: "Charlie",
-    job: "Janitor"
-  },
-  {
-    name: "Mac",
-    job: "Bouncer"
-  },
-  {
-    name: "Dee",
-    job: "Aspring actress"
-  },
-  {
-    name: "Dennis",
-    job: "Bartender"
-  }
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
 ];
 
 function MyApp() {
