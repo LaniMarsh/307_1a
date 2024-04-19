db302a562bfe3f8850c40b1f2d7f851510ee9b9c
LaniMarsh <122917983+LaniMarsh@users.noreply.github.com>
2024-04-18 17:17:54 -0700

    done with DELETE and name+job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,7 +40,59 @@ const users = {
     ]
   };
 
-app.get("/users", (req, res) => {
+  const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const { name, job } = req.query;
+    if (name && job) {
+      let result = users["users_list"].filter(
+        (user) => user.name === name && user.job === job
+      );
+      result = { users_list: result };
+      res.send(result);
+    } else if (name) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
+
+  const findUserByJob = (name) => {
+    return users["users_list"].filter(
+      (user) => user["job"] === job
+    );
+  };
+  
+
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const deleteUserById = (id) => {
+    const index = users["users_list"].findIndex((user) => user.id === id);
+    if (index !== -1) {
+      users["users_list"].splice(index, 1);
+    }
+  };
+  
+  app.delete("/users/:id", (req, res) => {
+    const id = req.params.id;
+    deleteUserById(id);
     res.send(users);
   });
 
@@ -49,3 +101,14 @@ app.listen(port, () => {
     `Example app listening at http://localhost:${port}`
   );
 });
+
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+  
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+  });
